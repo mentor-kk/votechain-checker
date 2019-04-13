32f6f0debd5e3b4cd83aaeb28902637b75202cbd
mentor-kk <mokatte@gmail.com>
2019-04-13 09:41:06 +0200

    Updates

---
+++ precache-manifest.ec891dac2ccf9576a2866552c66ccedc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "70b947403df66cbd9ca3",
+    "url": "/votechain-checker/static/js/runtime~main.ffb491c9.js"
+  },
+  {
+    "revision": "295d7de35177842c6451",
+    "url": "/votechain-checker/static/js/main.de9b0b75.chunk.js"
+  },
+  {
+    "revision": "775509bd2cd5e8756816",
+    "url": "/votechain-checker/static/js/2.68a65a0e.chunk.js"
+  },
+  {
+    "revision": "295d7de35177842c6451",
+    "url": "/votechain-checker/static/css/main.8088abe8.chunk.css"
+  },
+  {
+    "revision": "e4e30c82c692971f34e89547c02eefa5",
+    "url": "/votechain-checker/index.html"
+  }
+];
